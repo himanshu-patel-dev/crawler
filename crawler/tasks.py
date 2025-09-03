4b6c1702faca2e91d592caca0141565a682592ef
Himanshu Patel <py.himanshu.patel@gmail.com>
2025-09-03 22:50:12 +0530

    fix: remove "Accept-Encoding" header, since we don't handle 'br' encoding

--- crawler/tasks.py
+++ crawler/tasks.py
@@ -19,7 +19,6 @@ def fetch_url_task(self, url_id, url):
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 \
                     (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36",
         "Accept-Language": "en-US,en;q=0.9",
-        "Accept-Encoding": "gzip, deflate, br",
         "Referer": "https://www.amazon.com/"
     }
 
